8cf9ff52392a28d9e9ebe6e34437e038f9566d58
Noahxyzs <noahsolomon528@yahoo.com>
2022-12-08 12:35:06 -0800

    size of type

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -3,13 +3,15 @@
  * main - Entry point
  *
  * Return: Always 0 (success)
+ *
  */
 int main(void)
 {
-        printf("Size of a char: %lu byte(s)\n", sizeof(char));
-        printf("Size of a int: %lu byte(s)\n", sizeof(int));
-        printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
-        printf("Size of a long long int: %lu byte(s)\n", sizeof(long long int));
-        printf("Size of a float: %lu byte(s)\n", sizeof(float));
-        return (0);
+	printf("Size of a char: %lu byte(s)\n", sizeof(char));
+	printf("Size of a int: %i byte(s)\n", sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("size of a float: %f byte(s)\n", sizeof(float));
+	return (0);
+
 }
