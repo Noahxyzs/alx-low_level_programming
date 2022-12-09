fe252bf4475b8184ea3323d5cb190b22a4292272
Noahxyzs <noahsolomon528@yahoo.com>
2022-12-09 08:24:19 -0800

    capital and small

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char n;
+
+	char m;
+
+	n = 'a';
+	m = 'A';
+	while
+		(n <= 'z') {
+			putchar(n);
+			n++;
+		}
+	while
+		(m <= 'Z') {
+			putchar(m);
+			m++;
+		}
+	putchar('\n');
+	return (0);
+}
