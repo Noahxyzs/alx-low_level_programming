4746d65495479dd6289280ffbae40a28b021e777
Noahxyzs <noahsolomon528@yahoo.com>
2022-12-09 10:29:23 -0800

    hexa

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entery
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char n;
+
+	int i;
+
+	n = 'a';
+	i = 0;
+	while
+		(i < 10) {
+			putchar(i + '0');
+			i++;
+		}
+	while
+		(n <= 'f') {
+			putchar(n);
+			n++;
+		}
+	putchar('\n');
+	return (0);
+}
