a37b6a4424937bb22523d616b3dc78d0e66c8cd6
Noahxyzs <noahsolomon528@yahoo.com>
2022-12-09 08:08:01 -0800

    small letter

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	char n;
+
+	n = 'a';
+	while
+		(n <= 'z') {
+			putchar(n);
+			n++;
+		}
+	putchar('\n');
+	return (0);
+}
