eceb109a77c84afa42f99cdf990c24fcedfcaad1
Noahxyzs <noahsolomon528@yahoo.com>
2022-12-09 08:43:04 -0800

    except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - Entry
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+	char n;
+
+	n = 'a';
+	while
+		(n <= 'z') {
+			if (( n != 'q' && n != 'e') && n <= 'z')
+			putchar(n);
+			n++;
+		}
+	putchar('\n');
+	return (0);
+}
