8bee6816644a04630723a2d6b11de9100480f938
Noahxyzs <noahsolomon528@yahoo.com>
2022-12-13 07:39:49 -0800

    create -putchar

---
+++ 0x02-functions_nested_loops/_putchar.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+#include <unistd.h>
+/**
+ * main - return function
+ *
+ * using file guards
+ *
+ * _putchar - writes the character c to stdout
+ *
+ * @c: The character to print
+ *
+ * Return: success 1.
+ *
+ * On error, -1 is returned, and errno is set appropriate
+ *
+ * function _putchar create a function called _putchar
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
