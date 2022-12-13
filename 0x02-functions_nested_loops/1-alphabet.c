5e6bcd252278eff1a28ba7da22854deddab4463b
Noahxyzs <noahsolomon528@yahoo.com>
2022-12-13 08:12:42 -0800

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h'
+/**
+ * include main'h file guard
+ *
+ * create a function called print alphabet
+ */
+void print_alphabet(void)
+{
+	char c = 'a'
+
+		while(c <= 'z')
+		{
+			_putchar(c);
+			_putchar(c++);
+		}
+}
