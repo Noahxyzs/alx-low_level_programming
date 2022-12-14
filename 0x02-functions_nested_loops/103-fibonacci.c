eaf363378cf8a50644d7c57f667b9616502cc9fe
Noahxyzs <noahsolomon528@yahoo.com>
2022-12-13 17:22:37 -0800

    big fibonacci

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+/**
+ * main - prints
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+	long int a, b, j, sum;
+
+	a = 1;
+	b = 2;
+	sum = 0;
+	for (i = 1; i <= 33; ++i)
+	{
+		if (j < 4000000 && (a % 2) == 0)
+		{
+			sum = sum + a;
+		}
+		j = a + b;
+		a = b;
+		b = j;
+	}
+	printf("%lu\n", sum);
+	return (0);
+}
+
+
