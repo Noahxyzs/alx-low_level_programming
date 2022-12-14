0fc0316e2cec3d347e597f41e9046c7a807a2548
Noahxyzs <noahsolomon528@yahoo.com>
2022-12-13 18:28:14 -0800

    104

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -9,11 +9,11 @@ int main(void)
 	unsigned long int i;
 	unsigned long int a = 1;
 	unsigned long int b = 2;
-	unsigned long int j = 1000000000;
+	unsigned long int j = 10000000000;
 	unsigned long int a1, a2, b1, b2;
 
 	printf("%lu", a);
-	for (i = 1; i < 92; i++)
+	for (i = 1; i < 91; i++)
 	{
 		printf(", %lu", b);
 		b = b + a;
@@ -23,7 +23,7 @@ int main(void)
 	a2 = (a % j);
 	b1 = (b / j);
 	b2 = (b % j);
-	for (i = 93; i < 99; ++i)
+	for (i = 92; i < 99; ++i)
 	{
 		printf(", %lu", b1 + (b2 / j));
 		printf("%lu", b2 % j);
