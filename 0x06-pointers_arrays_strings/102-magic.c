783ceeae26dea0b92b3bd636667a7739cb124568
Noahxyzs <noahsolomon528@yahoo.com>
2022-12-22 11:56:03 -0800

    more pointer and array string

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -11,15 +11,15 @@ int main(void)
 	int *p;
 
 	a[2] = 1024;
-        p = &n;
-/*
- * wright ur code hear
- * You are not allowed to use the variable a in your new line of code
- * You are not allowed to modify the variable p
- * You can only write one statement
- * You are not allowed to use ,
- * You are not allowed to code anything else than the line of expected line of code at the expected line
- */
+	p = &n;
+	/*
+	 * * wright ur code hear
+	 * You are not allowed to use the variable a in your new line of code
+	 * You are not allowed to modify the variable p
+	 * You can only write one statement
+	 * You are not allowed to use ,
+	 * You are not allowed to code anything
+	 */
 	*(p + 5) = 98;
 	/*.......it prints 98 with new lign*/
 	printf("a[2] = %d\n", a[2]);
