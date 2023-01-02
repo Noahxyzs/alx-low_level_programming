25ea8ecb111dca6f922381aa77a463101de4eb92
Noahxyzs <noahsolomon528@yahoo.com>
2023-01-02 13:17:00 -0800

    Even more pointers, arrays and strings mandatory + adv

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _memset - a function 
+ * @s: add
+ * @b: the desired value
+ * @n: numbers to change
+ *
+ * Return: s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	int i = 0;
+
+	for (; n > 0; i++)
+	{
+		s[i] = b;
+		n--;
+	}
+	return (s);
+}
