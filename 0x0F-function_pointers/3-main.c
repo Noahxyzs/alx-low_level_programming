a801fe529b6f95f7feee693e140723ccfa784c09
Noahxyzs <noahsolomon528@yahoo.com>
2023-01-18 07:48:36 -0800

    it often serves simply as something to aim at

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -13,6 +13,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 {
 	int num1, num2;
 	char *op;
+
 	if (argc != 4)
 	{
 		printf("Error\n");
@@ -26,7 +27,7 @@ int main(int __attribute__((__unused__)) argc, char *argv[])
 		printf("Error\n");
 		exit(99);
 	}
-	if ((*op == '/' && num2 == 0) ||(*op == '%' && num2 == 0))
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -22,7 +22,7 @@ int op_add(int a, int b)
  * @b: para
  * Return: subtruction result
  */
-int op_sub(int a, int b)	
+int op_sub(int a, int b)
 {
 	return (a - b);
 }
