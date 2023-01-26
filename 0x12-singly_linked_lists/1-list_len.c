6eedd5e9c52aa41f747f43a351f36b7f78d42762
Noahxyzs <noahsolomon528@yahoo.com>
2023-01-26 10:07:16 -0800

    singly linked lists

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,12 +3,13 @@
 
 /**
  * list_len - linked list
- * @h: pointer to the list_t list
+ * @h: pointer list
  * Return: n
  */
 size_t list_len(const list_t *h)
 {
 	size_t n = 0;
+
 	while (h)
 	{
 		n++;
